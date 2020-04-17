67e6880c2dc257c8ab051b2837abaf98c8ea4215
Roger Zavarce <rogerzavarce@gmail.com>
2020-04-17 13:23:52 +0200

    Unit Test para las API de Author y Articles

--- TangramTestConf/settings.py
+++ TangramTestConf/settings.py
@@ -159,6 +159,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [                     
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+    'TEST_REQUEST_RENDERER_CLASSES': [
+        'rest_framework.renderers.MultiPartRenderer',
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.TemplateHTMLRenderer'
+    ]
 }
 
 
--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework.test import RequestsClient
 
-# Create your tests here.
+from .models import Article
+
+import requests, json
+
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
+
+from django.urls import reverse
+
+
+class ArticleTestCase(APITestCase):
+	
+	def test_get_list(self):
+
+		client = RequestsClient()
+		response = client.get('http://127.0.0.1:8000/api/v1/articles/')
+		assert response.status_code == 200
+
+	def test_post_article(self):
+
+		url='http://127.0.0.1:8000/api/v1/articles/'
+		data= {"id":100,"art_title":"Esta es una prueba","art_content":"Esta es una prueba y su contenido","art_status":1}
+
+		client = RequestsClient()
+		response = client.post(url, data)
+
+	def test_get_article(self):
+
+		url='http://127.0.0.1:8000/api/v1/articles/1/'
+
+		client = RequestsClient()
+		response = client.get('http://127.0.0.1:8000/api/v1/articles/1/?format=json')
+
+
+	def test_put_article(self):
+
+		url='http://127.0.0.1:8000/api/v1/articles/1/	'
+		data= {"id":1,"art_title":"qwqwqwqw111111","art_content":"qwqwqwqwqw","art_status":1}
+
+		client = RequestsClient()
+		response = client.put(url, data)
+
+	def test_del_article(self):
+
+		url='http://127.0.0.1:8000/api/v1/articles/1/'
+
+		client = RequestsClient()
+		response = client.delete('http://127.0.0.1:8000/api/v1/articles/1/?format=json')
+
+
